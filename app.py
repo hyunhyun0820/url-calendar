238432c78bb0ee1bced86e5cf4c38f96e73fd6e4
hyunhyun0820 <hyunhyun0820@naver.com>
2025-04-05 22:44:22 +0900

    Render 배포를 위해 포트 환경변수 적용

--- app.py
+++ app.py
@@ -121,6 +121,7 @@ def move_box(data):
         db.session.commit()
         emit('move_box', data, broadcast=True)
 
-# 앱 실행
+#실행
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
